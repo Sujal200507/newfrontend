8cadbe781d67b67f6e7dbc4751ec1dce846fba3d
Your Sp2089 Username <youremail@example.com>
2025-07-10 00:03:10 +0530

    Final Commit

--- src/Components/BackendLoader.jsx
+++ src/Components/BackendLoader.jsx
@@ -2,15 +2,11 @@
 
 function BackendLoader() {
   return (
-    <div className="w-full h-screen bg-[#0D1117] flex items-center justify-center px-4 text-center">
-    <h2 className="text-[#8B949E] text-2xl font-bold leading-relaxed">
-      Setting up the environment for you.<br />
-      <span className="text-xl font-semibold text-[#8B949E] block mt-4">
+    <div className="w-full h-screen bg-[#0D1117] flex items-center justify-center ">
+        <h2 className="text-[#8B949E]">Setting up the environment for you.</h2>
         💤 The backend server was asleep due to inactivity.<br />
-        It's now restarting — please wait a few seconds.....
-      </span>
-    </h2>
-  </div>
+      It's now restarting — please wait a few seconds...
+    </div>
   )
 }
 
