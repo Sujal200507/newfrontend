3afd27d332f2dba7ec22d6172d3f15445874d736
Your Sp2089 Username <youremail@example.com>
2025-07-09 23:57:04 +0530

    Final Commit

--- src/Components/BackendLoader.jsx
+++ src/Components/BackendLoader.jsx
@@ -2,9 +2,15 @@
 
 function BackendLoader() {
   return (
-    <div className="w-full h-screen bg-[#0D1117] flex items-center justify-center ">
-        <h2 className="text-[#8B949E]">Setting up the environment for you.</h2>
-    </div>
+    <div className="w-full h-screen bg-[#0D1117] flex items-center justify-center px-4 text-center">
+    <h2 className="text-[#8B949E] text-2xl font-bold leading-relaxed">
+      Setting up the environment for you.<br />
+      <span className="text-xl font-semibold text-[#8B949E] block mt-4">
+        💤 The backend server was asleep due to inactivity.<br />
+        It's now restarting — please wait a few seconds...
+      </span>
+    </h2>
+  </div>
   )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()]
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      plugins: [visualizer({ open: true })]
+    }
+  }
 })
